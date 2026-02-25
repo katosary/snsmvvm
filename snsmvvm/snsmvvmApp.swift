ee45c91fc22f72ecf4318d43d36984533bf3d8a4
Kato Soma <katos.arys@gmail.com>
2026-02-25 21:34:10 +0900

    Initial Commit

---
+++ snsmvvm/snsmvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  snsmvvmApp.swift
+//  snsmvvm
+//
+//  Created by katoso on 2026/02/25.
+//
+
+import SwiftUI
+
+@main
+struct snsmvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ snsmvvmTests/snsmvvmTests.swift
@@ -0,0 +1,17 @@
+//
+//  snsmvvmTests.swift
+//  snsmvvmTests
+//
+//  Created by katoso on 2026/02/25.
+//
+
+import Testing
+@testable import snsmvvm
+
+struct snsmvvmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
